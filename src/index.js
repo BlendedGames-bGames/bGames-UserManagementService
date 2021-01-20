2cb4c9c70985bf2649c2b3aa24d278c45f1c9627
Tisks <leiser.mahu@usach.cl>
2021-01-20 11:31:23 -0300

    Changing port number

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 //Settings
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3010;
 
 //Middlewares
 app.use(express.json());
