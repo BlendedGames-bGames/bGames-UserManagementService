73516031eb54e698f715f6a43e0cec3b12c71db8
Tisks <leiser.mahu@usach.cl>
2021-01-20 22:15:06 -0300

    HOTFIX variable name

--- src/database.js
+++ src/database.js
@@ -9,7 +9,7 @@ var mysqlConnection  = mysql.createPool({
     multipleStatements:true
 });
   
-  pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+mysqlConnection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
     if (error) throw error;
     console.log('The solution is: ', results[0].solution);
   });
