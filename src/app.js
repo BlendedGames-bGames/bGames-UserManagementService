5f15696ca09bb4dfa412b675dc52294570ebdab3
Tisks <leiser.mahu@usach.cl>
2021-01-29 17:53:47 -0300

    app.use(cors())
    NOTE: When using this middleware as an application level middleware (for example, app.use(cors())), pre-flight requests are already handled for all routes.

--- src/app.js
+++ src/app.js
@@ -14,10 +14,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-    origin: ["*"],
-    methods: ["POST", "GET","PUT"]
-}))
+app.use(cors())
+
 //Routes
 app.use(player_config);
 
