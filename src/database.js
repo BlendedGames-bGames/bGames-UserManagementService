99b10cba407ca6e247dde9fd7232bcd58428d68f
Tisks <leiser.mahu@usach.cl>
2021-02-08 20:36:21 -0300

    Error: Handshake inactivity timeout
    https://stackoverflow.com/questions/35553432/error-handshake-inactivity-timeout-in-node-js-mysql-module
    
    FIX: More time for the handshake in config of the pool

--- src/database.js
+++ src/database.js
@@ -1,7 +1,10 @@
 const mysql = require('mysql');
 
 var mysqlConnection  = mysql.createPool({
-    connectionLimit : 100,
+    connectionLimit : 1000,
+    connectTimeout  : 60 * 60 * 1000,
+    acquireTimeout  : 60 * 60 * 1000,
+    timeout         : 60 * 60 * 1000,
     host:'db4free.net',
     user:'grybyus',
     password:'interaction1',
