6b942db94dfba08073aa24f98ce9937849876ee0
Tisks <leiser.mahu@usach.cl>
2021-02-15 21:02:02 -0300

    Replacing for environment variables

--- src/database.js
+++ src/database.js
@@ -5,11 +5,11 @@ var mysqlConnection  = mysql.createPool({
     connectTimeout  : 60 * 60 * 1000,
     acquireTimeout  : 60 * 60 * 1000,
     timeout         : 60 * 60 * 1000,
-    host:'bgames-MysqlDB',
-    port:'3306',
-    user:'foo',
-    password:'Bnx69t304857@.@',
-    database: 'blended_games',
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     multipleStatements:true
 });
 
